9577f4ed70a1b86cb921cb19571279b80ced2578
Lee Saltzman <mccall.saltzman@gmail.com>
2017-05-03 21:35:02 -0400

    header and selection

--- AutoOMPPass/AutoOMPPass.cpp
+++ AutoOMPPass/AutoOMPPass.cpp
@@ -1,10 +1,17 @@
 #include "llvm/Pass.h"
+#include "llvm/IR/Dominators.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/InstVisitor.h"
+#include "llvm/Analysis/LoopInfo.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/Transforms/Utils/CodeExtractor.h"
+#include "llvm/IR/IRBuilder.h"
+#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace llvm;
 
@@ -15,110 +22,175 @@ namespace {
         AutoOMPPass() : ModulePass(ID) {}
 
 
-        std::vector<Loop*> getParallelizableLoops(LoopInfo &LI){
+      std::vector<Loop*> getParallelizableLoops(LoopInfo &LI){
 			std::vector<Loop*> parallelizableLoops;
 
 			for(LoopInfo::iterator iter = LI.begin(), itend = LI.end(); iter != itend; ++iter){
-				if(iter->getUniqueExitBlock() == nullptr) continue;
+				if((*iter)->getUniqueExitBlock() == nullptr) continue;
+        bool shouldParallelize = true;
+        for(Loop::block_iterator BI = (*iter)->block_begin(), BE = (*iter)->block_end(); BI != BE; ++BI){
+          for(BasicBlock::iterator II = (*BI)->begin(), IE = (*BI)->end(); II != IE; ++II){
+            if(isa<CallInst>(II)){
+              shouldParallelize = false;
+              break;
+            }
+          }
+          if(!shouldParallelize) break;
+        }
 				//add other checks here
-				parallelizableLoops.push_back(static_cast<Loop*>(iter));
+				parallelizableLoops.push_back(static_cast<Loop*>(*iter));
 			}
 
 
 		}
 
 		//loop extractor: modified from "LoopExtractor.cpp" in the llvm source
-        Function* extractLoop(Loop *L, DominatorTree &DT, LoopInfo &LI) {
-            if (skipOptnoneFunction(L))
-                return false;
- 
-  			 // Only visit top-level loops.
-   			if (L->getParentLoop())
-    			return false;
- 
-   			// If LoopSimplify form is not available, stay out of trouble.
-   			if (!L->isLoopSimplifyForm())
-    			return false;
- 
-   			bool Changed = false;
- 
-		   // If there is more than one top-level loop in this function, extract all of
-   		 // the loops. Otherwise there is exactly one top-level loop; in this case if
-  		 // this function is more than a minimal wrapper around the loop, extract
-  		 // the loop.
-   			bool ShouldExtractLoop = false;
- 
-   		// Extract the loop if the entry block doesn't branch to the loop header.
-	   		TerminatorInst *EntryTI = L->getHeader()->getParent()->getEntryBlock().getTerminator();
-  		
-			 if (!isa<BranchInst>(EntryTI) ||  !cast<BranchInst>(EntryTI)->isUnconditional() || EntryTI->getSuccessor(0) != L->getHeader()) {
-    	 		ShouldExtractLoop = true;
-   			} else {
-     		// Check to see if any exits from the loop are more than just return
-     		// blocks.
-    	 		SmallVector<BasicBlock*, 8> ExitBlocks;
-     			L->getExitBlocks(ExitBlocks);
-     			for (unsigned i = 0, e = ExitBlocks.size(); i != e; ++i)
-     			    if (!isa<ReturnInst>(ExitBlocks[i]->getTerminator())) {
-        	 			ShouldExtractLoop = true;
-        	 			break;
-       				}
+    Function* extractLoop(Loop *L, DominatorTree &DT, LoopInfo &LI) {
+			 // Only visit top-level loops.
+            std::cerr << "HERE a" << std::endl;
+ 			if (L->getParentLoop())
+  			return nullptr;
+
+ 			// If LoopSimplify form is not available, stay out of trouble.
+ 			if (!L->isLoopSimplifyForm())
+  			return nullptr;
+      std::cerr << "HERE b" << std::endl;
+
+ 			bool Changed = false;
+
+	   // If there is more than one top-level loop in this function, extract all of
+ 		 // the loops. Otherwise there is exactly one top-level loop; in this case if
+		 // this function is more than a minimal wrapper around the loop, extract
+		 // the loop.
+ 			bool ShouldExtractLoop = false;
+      std::cerr << "HERE1 " << std::endl;
+
+ 		// Extract the loop if the entry block doesn't branch to the loop header.
+   		TerminatorInst *EntryTI = L->getHeader()->getParent()->getEntryBlock().getTerminator();
+		  std::cerr << "HERE 2" << std::endl;
+		 if (!isa<BranchInst>(EntryTI) ||  !cast<BranchInst>(EntryTI)->isUnconditional() || EntryTI->getSuccessor(0) != L->getHeader()) {
+  	 		ShouldExtractLoop = true;
+ 			} else {
+   		// Check to see if any exits from the loop are more than just return
+   		// blocks.
+  	 		SmallVector<BasicBlock*, 8> ExitBlocks;
+   			L->getExitBlocks(ExitBlocks);
+   			for (unsigned i = 0, e = ExitBlocks.size(); i != e; ++i)
+   			    if (!isa<ReturnInst>(ExitBlocks[i]->getTerminator())) {
+      	 			ShouldExtractLoop = true;
+      	 			break;
+     				}
+ 			}
+
+ 			if (ShouldExtractLoop) {
+  		 // We must omit EH pads. EH pads must accompany the invoke
+  		 // instruction. But this would result in a loop in the extracted
+   		// function. An infinite cycle occurs when it tries to extract that loop as
+   		// well.
+ 	  			SmallVector<BasicBlock*, 8> ExitBlocks;
+  	 		L->getExitBlocks(ExitBlocks);
+   			for (unsigned i = 0, e = ExitBlocks.size(); i != e; ++i)
+   				if (ExitBlocks[i]->isEHPad()) {
+       				ShouldExtractLoop = false;
+      	 			break;
+     				}
+ 			}
+			  Function* extracted = nullptr;
+ 			if (ShouldExtractLoop) {
+  			
+ 			  CodeExtractor Extractor(DT, *L);
+			  extracted = Extractor.extractCodeRegion();
+   			if (extracted != nullptr) {
+     				Changed = true;
+     			// After extraction, the loop is replaced by a function call, so
+    			 // we shouldn't try to run any more loop passes on it.
+     				LI.markAsRemoved(L);
    			}
- 
-   			if (ShouldExtractLoop) {
-    		 // We must omit EH pads. EH pads must accompany the invoke
-    		 // instruction. But this would result in a loop in the extracted
-     		// function. An infinite cycle occurs when it tries to extract that loop as
-     		// well.
-   	  			SmallVector<BasicBlock*, 8> ExitBlocks;
-    	 		L->getExitBlocks(ExitBlocks);
-     			for (unsigned i = 0, e = ExitBlocks.size(); i != e; ++i)
-     				if (ExitBlocks[i]->isEHPad()) {
-         				ShouldExtractLoop = false;
-        	 			break;
-       				}
-   			}
- 			Function* extracted = nullptr;
-   			if (ShouldExtractLoop) {
-    			 if (NumLoops == 0) return Changed;
-    			 --NumLoops;
-     			CodeExtractor Extractor(DT, *L);
-				extracted = Extractor.extractCodeRegion();
-     			if (extracted != nullptr) {
-       				Changed = true;
-       			// After extraction, the loop is replaced by a function call, so
-      			 // we shouldn't try to run any more loop passes on it.
-       				LI.markAsRemoved(L);
-     			}
-     			++NumExtracted;
-   			}
+ 			}
  			return extracted;
 		}
 
+    void createHeader(Function* extracted){
+      BasicBlock* entry = &extracted->getEntryBlock();
+
+      BasicBlock* header = BasicBlock::Create(entry->getContext(), "", extracted, entry);
+      IRBuilder<> builder(header);
+      Type* i32type = Type::getInt32PtrTy(header->getContext());
+      auto global_tid = builder.createAlloca(i32type);
+      auto bound_tid = builder.createAlloca(i32type);
+      auto c_addr = builder.createAlloca(i32type);
+      std::vector<Instruction*> arrays;
+      int num_iterations= 0;
+      for(Function::iterator BB = extracted->begin(); BB != extracted->end(); ++BB){
+        for(BasicBlock::iterator Inst = BB->begin(); Inst != BB->end(); ++Inst){
+          if(isa<GetElementPtrInst>(Inst)){
+            arrays.push_back(builder.createAlloca(i32type, cast<GetElementPtrInst>(Inst)->getNumIndices());
+              num_iterations = cast<GetElementPtrInst>(Inst)->getNumIndices();
+          }
+        }
+      }
+
+      auto iv = builder.createAlloca(i32type);
+      auto lb =builder.createAlloca(i32type);
+      auto ub = builder.createAlloca(i32type);
+      auto stride = builder.createAlloca(i32type);
+      auto islast = builder.createAlloca(i32type);
+
+      //extract function made sure the loop was cannonicallized
+      builder.createStore(0, lb);
+      builder.createStore(num_iterations - 1, ub);
+      builder.createStore(1, stride);
+      builder.createStore(0, islast);
+
+      //
+
+    }
 		
-        bool runOnModule(Module &M) {
-            LoopInfo &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
-			std::vector<Loop*> loops = getParallelizableLoops(LI);
-			
-			for(std::vector<Loop*>::iterator iter = loops.begin(), iend = loops.end(); iter != iend; ++iter){
-				Function* extracted = extractLoop(iter);
-				if(!extracted) continue;
-				//create header, function call for loop;
-			}
+    bool runOnModule(Module &M) {
+      std::cerr << "AUTO OMP PASS" << std::endl;
+
+      for(Module::iterator fiter = M.getFunctionList().begin(); fiter != M.getFunctionList().end(); ++fiter){
+
+            LoopInfo &LI = getAnalysis<LoopInfoWrapperPass>(*fiter).getLoopInfo();
+      			std::vector<Loop*> loops = getParallelizableLoops(LI);
+      			DominatorTree &DT = getAnalysis<DominatorTreeWrapperPass>(*fiter).getDomTree();
+            M.dump();
+
+      			for(std::vector<Loop*>::iterator iter = loops.begin(), iend = loops.end(); iter != iend; ++iter){
+              (*iter)->dump();
+              BasicBlock pred = (*iter)->getLoopPredecessor();
 
 
-            return false;
-        }
-        void getAnalysisUsage(AnalysisUsage &AU) const {
-            AU.addRequired<LoopInfoWrapperPass>();
-        }
-    };
+      				Function* extracted = extractLoop(*iter, DT, LI);
+      				if(!extracted) continue;
+
+
+
+              IRBuilder<> builder(pred);
+
+              builder.CreateCall(extracted);
+
+
+              createHeader(extracted);
+
+      				//create header, function call for loop;
+      			}
+            M.dump();
+          }
+      std::cerr << "END AUTO OMP PASS" << std::endl;
+
+      return false;
+    }
+    void getAnalysisUsage(AnalysisUsage &AU) const {
+        AU.addRequired<LoopInfoWrapperPass>();
+        AU.addRequired<DominatorTreeWrapperPass>();
+    }
+  };
 }
 
 char AutoOMPPass::ID = 0;
 
-static void AutoOMPPass(const PassManagerBuilder &, legacy::PassManagerBase &PM) {
+static void registerAutoOMPPass(const PassManagerBuilder &, legacy::PassManagerBase &PM) {
     PM.add(new AutoOMPPass());
 }
-h
-static RegisterPass<AutoOMPPass> X("reverseInst" , "Inst Reverse Pass", false, false);
+static RegisterPass<AutoOMPPass> X("autoOMP" , "Auto OMP Pass", false, false);
---
+++ build/AutoOMPPass/CMakeFiles/AutoOMPPass.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AutoOMPPass.dir/AutoOMPPass.cpp.o"
+  "libAutoOMPPass.pdb"
+  "libAutoOMPPass.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AutoOMPPass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
